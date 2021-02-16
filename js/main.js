6d4841b4531ae7f76819b1381010e31831dd4da7
Jorge Torres A <jorgetorresa@MacBook-Pro-de-Jorge.local>
2021-02-15 20:44:30 -0600

    delete foreach

--- js/main.js
+++ js/main.js
@@ -3,8 +3,3 @@ var colores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","B
 
 // Vector con todos los adornos
 var adornos = document.getElementsByClassName("adorno");
-
-// Asignamos el evento click a cada adorno con la funcion de cambiar color
-for( var i=0; i<adornos.length; i++){
-    adornos[i].addEventListener('click', changeColorBackground, false);
-}
