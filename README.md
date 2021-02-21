a42dddf10fa12796ea06c11834a4eefba408a014
dacord <dcordero014@gmail.com>
2021-02-21 12:58:01 -0600

    version1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Árbol de Navidad
+Un pequeño experimento en JavaScript para el evento 
+
+##Temas abordados
+- HTML y CSS
+- Variables
+- Vectores
+- Funciones
+- Eventos
---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Vector con colores (Tomados de https://gist.github.com/bobspace/2712980)
+var colores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+// Vector con todos los adornos
+var adornos = document.getElementsByClassName("adorno");
+let miAdorno = document.getElementById("adorno-3");
+// Asignamos el evento click a cada adorno con la funcion de cambiar color
+
+miAdorno.addEventListener("mouseover", () => {
+    miAdorno.style.backgroundColor ="Aquamarine";
+});
+
+
+ var tomate = document.getElementById("adorno-2");
+
+ function prender() {
+     tomate.style.background = "red";
+ }
+
+ tomate.addEventListener("click", function() {
+    prender();
+ });
